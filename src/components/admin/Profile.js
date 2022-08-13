c3494acddf4a1fdc17c9e919d936bac0b1502752
mohasal0101 <mohasal0101@gmail.com>
2022-08-13 14:37:04 +0300

    salah-carousel-adminprofile

--- src/components/admin/Profile.js
+++ src/components/admin/Profile.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react'
+// "Hi admin" with a drop down menu, contains the options of: view admin info and sign out option (when the sign out pressed, redirect to the user home page)
+class Profile extends Component {
+    render() {
+        return (
+            <div className="profile">
+                <div className="profile-header">
+                    <h1>Hi admin</h1>
+                    <div className="profile-dropdown">
+                        <div className="profile-dropdown-item">
+                            <a href="/admin/profile">View admin info</a>
+                        </div>
+                        <div className="profile-dropdown-item">
+                            <a href="/">Sign out</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="profile-body">
+                    <div className="profile-body-item">
+                        <h2>Admin info</h2>
+                        <p>Name: admin</p>
+                        <p>Email:
+                            <a href="mailto:
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div>"Hi admin" with a drop down menu, contains the options of: view admin info and sign out option (when the sign out pressed, redirect to the user home page)</div>
-    )
-  }
-}
+                            ">
+
+                            </a>
+                        </p>
+                        <p>Phone:
+                            <a href="tel:
+
+                            ">
+
+                            </a>
+                        </p>
+                    </div>
+                    <div className="profile-body-item">
+                        <h2>Admin info</h2>
+                        <p>Name: admin</p>
+                        <p>Email:
+                            <a href="mailto:
+
+                            ">
+
+                            </a>
+                        </p>
+                        <p>Phone:
+                            <a href="tel:
+
+                            ">
+
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
