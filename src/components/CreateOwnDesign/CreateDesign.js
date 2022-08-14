cf4cff963a806e6036893dca2f3a79036843046d
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-08-14 10:27:10 +0300

    create footer & add form in createDesign.js

--- src/components/CreateOwnDesign/CreateDesign.js
+++ src/components/CreateOwnDesign/CreateDesign.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Styling/createDesign.css';
 
 export default class CreateDesign extends Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target.url.value;
-        console.log(form);
+  constructor(props) {
+    super(props);
+    this.state = {
+      userUrl: ''
     }
+  }
+
+  formInput = (e) => {
+    e.preventDefault();
+    if (e.target.inputUrl.value !== ""){
+    this.setState({userUrl: e.target.inputUrl.value })
+  }else {
+   alert(`Please enter a valid url !`)
+  }
+  }
+ 
+
   render() {
     return (
-      <div>
-        <Form className="mb-3" onSubmit={this.handleSubmit}>
-          <Form.Group controlId="url">
-            <Form.Label>Enter a valid url for your image</Form.Label>
-            <Form.Control type="text" placeholder="Enter url"/>
-            <Form.Text className="text-muted">
-              We'll never share your data with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Button variant="primary" type="submit" value="Submit">
-        Submit
-      </Button>
+      <div className='createdesign'>
+        <div>
+          <Form onSubmit={(e)=>{this.formInput(e)}} id="inputUrl">
+            <Form.Group className="mb-3">
+              <Form.Label>Image URL</Form.Label>
+              <Form.Control id="inputUrl" type="url" placeholder="Enter url ..." />
+              <Button className='button' variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form.Group>
           </Form>
-        </div>
+        </div> 
+        <div >
+        <Card style={{ width: '18rem' }} className= "cardSt" >
+          { this.state.userUrl !== "" &&
+            <Card.Img variant="top" src={this.state.userUrl} alt="error 404" />
+          }
+          </Card>
+      </div>
+      </div>
     )
   }
 }
+
+/*Form for the user to enter the image URL and a submit button. 
+then send axios request to removeBG for the required image to render it in 
+the card shown below the form*/
