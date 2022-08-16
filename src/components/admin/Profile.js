b425753cc948c302d023544d195cfb47871d97db
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-08-16 13:26:53 +0300

    added more styling, and fixed some bugs

--- src/components/admin/Profile.js
+++ src/components/admin/Profile.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import '../../Styling/Profile.css';
+
+
 class Profile extends Component {
   render () {
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://i.ibb.co/6XMk94T/admin.jpg" />
+      <div className="admin-profile">
+      <Card style={{ width: '18rem' }} className="admin-card">
         <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
+        <Card.Img variant="top" src="https://i.ibb.co/6XMk94T/admin.jpg" className="profile__img" />
+          <Card.Title className="profile__name"></Card.Title>
+          <Card.Text className='profile__occupation'>
             Username: Admin<br />
             Password: Admin
           </Card.Text>
+          <Card.Text className='profile__availability'>
+          <div className="circle"></div>
+           <span>Available</span>
+          </Card.Text>
         </Card.Body>
       </Card>
+      </div>
     );
   }
 };
