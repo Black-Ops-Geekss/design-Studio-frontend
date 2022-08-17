ba89943a9df01a158dabf1b33622b35801e64483
Zaid alshibi <zaidealshibi@gmail.com>
2022-08-18 02:19:36 +0300

    adding empty orders

--- src/components/admin/ManageOrders.js
+++ src/components/admin/ManageOrders.js
@@ -49,43 +49,46 @@ export default class ManageOrders extends Component {
   render () {
     return (
       <>
-        <div >
-        <Container className='orders'>
-
-        {this.state.ordersArray.filter( filterDesign => filterDesign.items !== null ).map( design => (
-            <Card style={{ width: '20rem' }} className="card-order">
-              <Carousel fade>
-                {design.items.map( url => (
-
-                  <Carousel.Item>
-                    <img
-                      className="d-block"
-                      src={url}
-                      alt="imageOrder"
-                      height="300"
-                      width="400"
-                    />
-                  </Carousel.Item >
-                ) )}
-              </Carousel >
-              <Card.Body className='card-body-order'>
-                <Card.Title>{design.name}</Card.Title>
-                <Card.Text>
-                 Phone: {design.phone}<br />
-                 Email: {design.email}<br />
-                 City: {design.location}
-                </Card.Text>
-                <Button onClick={() => this.deleteOrder( design._id )} className="button-card-2">Delete order</Button>
-                <Button onClick={() => this.confirmOrder( design._id )} className="button-card-2"  style={{marginLeft:'25px'}}>Confirm Order</Button>
-
-              </Card.Body>
-            </Card>
-        ) )}
-                  </Container>
-
+        {this.state.ordersArray.length === 0 ?(
+          <div className='empty'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Hourglass_icon.png" alt="empty" />
+          <h1>
+            You Don't Have Any Orders Right Now
+          </h1>
           </div>
-
-
+        ) : (
+          <div >
+            <Container className='orders'>
+              {this.state.ordersArray.filter( filterDesign => filterDesign.items !== null ).map( design => (
+                <Card style={{ width: '20rem' }} className="card-order">
+                  <Carousel fade>
+                    {design.items.map( url => (
+                      <Carousel.Item>
+                        <img
+                          className="d-block"
+                          src={url}
+                          alt="imageOrder"
+                          height="300"
+                          width="400"
+                        />
+                      </Carousel.Item >
+                    ) )}
+                  </Carousel >
+                  <Card.Body className='card-body-order'>
+                    <Card.Title>{design.name}</Card.Title>
+                    <Card.Text>
+                      Phone: {design.phone}<br />
+                      Email: {design.email}<br />
+                      City: {design.location}
+                    </Card.Text>
+                    <Button onClick={() => this.deleteOrder( design._id )} className="button-card-2">Delete order</Button>
+                    <Button onClick={() => this.confirmOrder( design._id )} className="button-card-2" style={{ marginLeft: '25px' }}>Confirm Order</Button>
+                  </Card.Body>
+                </Card>
+              ) )}
+            </Container>
+          </div>
+        )}
       </>
     );
   };
